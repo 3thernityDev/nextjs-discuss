2040fda66e655ef26ddb37adfa56bd863d225874
3thernitydev <3thernitydev@gmail.com>
2026-05-12 10:07:57 +0400

    feat(homePage): Ajout du bouton commencer à discuter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+    const router = useRouter();
+
+    const handleStartChatting = () => {
+        router.push("/chat");
+    };
+
     return (
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Exemple
-        </h1>
+        <div className="flex flex-col items-center justify-center h-screen gap-6">
+            <h1 className="text-3xl font-semibold">
+                Bienvenue sur DiscussLike !
+            </h1>
+
+            <button
+                onClick={handleStartChatting}
+                className="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600 cursor-pointer"
+            >
+                Commencer à discuter
+            </button>
+        </div>
     );
 }
